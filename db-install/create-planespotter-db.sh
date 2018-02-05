899707c322ac6815c6348cf8b4e231bf99f831b2
Sushil Singh <sushils@vmware.com>
2018-02-05 15:28:56 -0800

    Converted all the services to work under k8s.
    Signed-off-by: Sushil Singh <sushilks@gmail.com>

--- db-install/create-planespotter-db.sh
+++ db-install/create-planespotter-db.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
+set -x
 
+export MYSQL_ROOT_PASSWORD=planspotter-sql
+cd /
+pwd
+#/usr/local/bin/docker-entrypoint.sh mysqld &
+mysqld -uroot &
+sleep 5
 cd ~/planespotter/db-install/
-wget http://registry.faa.gov/database/ReleasableAircraft.zip
+#wget http://registry.faa.gov/database/ReleasableAircraft.zip
 unzip ReleasableAircraft.zip
-rm ReleasableAircraft.zip DEALER.txt DEREG.txt DOCINDEX.txt ENGINE.txt RESERVED.txt
-mysql -u root -p < create-planespotter-db.sql
+ps -eaf
+#rm ReleasableAircraft.zip DEALER.txt DEREG.txt DOCINDEX.txt ENGINE.txt RESERVED.txt
+rm DEALER.txt DEREG.txt DOCINDEX.txt ENGINE.txt RESERVED.txt
+mysql --user=root --password=${MYSQL_ROOT_PASSWORD} < create-planespotter-db.sql
 rm MASTER.txt ACFTREF.txt
+#killall -9 mysqld
